43a14e68886e63cff4618ea2e6ea5a425a2f8ccd
kuzkovandrey <kuzkovandrey@alignedcode.com>
2022-03-24 21:59:55 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'FoodAndRecipes',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
